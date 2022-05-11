12e679cfac46bfff234642f56d50a43de6838b9b
Małgorzata Szwed <84782927+Swedeling@users.noreply.github.com>
2022-05-11 21:56:22 +0200

    Update userBazaLekow.php

--- userBazaLekow.php
+++ userBazaLekow.php
@@ -15,7 +15,7 @@
     session_start();
     $servername = "mysql.agh.edu.pl";
     $username = "telesins";
-    $password = "PWoHPsG1JofSzWm7";
+    $password = "********";
     $dbname = "telesins";
     $uzytkownik = $_SESSION["current_user"];
     $idapteczki = $_SESSION["current_apteczka"];
